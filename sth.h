46899cedb32484378648917caa3df2d4fa020c0b
thehxdev <hossein.khosravi.ce@gmail.com>
2025-12-31 16:09:38 +0330

    version 0.2.3
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- sth.h
+++ sth.h
@@ -3,8 +3,8 @@
 
 #define STH_VERSION_MAJOR 0
 #define STH_VERSION_MINOR 2
-#define STH_VERSION_PATCH 2
-#define STH_VERSION_STRING "0.2.2"
+#define STH_VERSION_PATCH 3
+#define STH_VERSION_STRING "0.2.3"
 
 #if !defined(__cplusplus) && (!defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L)
     #error "At least C99 standard is required"
