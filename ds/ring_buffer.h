1ca0935fc55c06e913dbbb75ecc64ec8871c576f
thehxdev <hossein.khosravi.ce@gmail.com>
2025-12-31 16:09:07 +0330

    fix: fixed ring buffer macros
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- ds/ring_buffer.h
+++ ds/ring_buffer.h
@@ -3,12 +3,12 @@
 
 // https://www.snellman.net/blog/archive/2016-12-13-ring-buffers/
 
-#define sth_ds_ring_buffer_mask(v, cap) ((v) & ((cap) - 1))
+#define sth_ds_ring_buffer_mask(cap, v) ((v) & ((cap) - 1))
 
 #define sth_ds_ring_buffer_init(rb, backing_buffer, capacity) \
     do { \
-        STH_BASE_ASSERT((capacity & (capacity-1)) == 0 && "ring buffer capacity must be a power of 2"); \
         size_t _cap_ = (capacity); \
+        STH_BASE_ASSERT((_cap_ & (_cap_-1)) == 0 && "ring buffer capacity must be a power of 2"); \
         (rb)->items = STH_BASE_DECLTYPE((rb)->items) (backing_buffer); \
         (rb)->read = 0; \
         (rb)->write = 0; \
@@ -26,17 +26,17 @@
 
 #define sth_ds_ring_buffer_push(rb, item) \
     ((!sth_ds_ring_buffer_is_full((rb))) \
-        ? ((rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->write++)] = (item), STH_OK) \
+        ? ((rb)->write += 1, (rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->write)] = (item), STH_OK) \
         : STH_FAILED)
 
 #define sth_ds_ring_buffer_shift(rb) \
     ((!sth_ds_ring_buffer_is_empty((rb))) \
-        ? ((rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->read++)], STH_OK) \
+        ? ((rb)->read += 1, (rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->read)], STH_OK) \
         : STH_FAILED)
 
 #define sth_ds_ring_buffer_shift_out(rb, out) \
     ((!sth_ds_ring_buffer_is_empty((rb))) \
-        ? ((out) = (rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->read++)], STH_OK) \
+        ? ((rb)->read += 1, (out) = (rb)->items[sth_ds_ring_buffer_mask((rb)->cap, (rb)->read)], STH_OK) \
         : STH_FAILED)
 
 #endif // _STH_DS_RING_BUFFER_H_
